db9f6af18f93fdd8f378f2585fb96d1ae3f1f757
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-10-02 00:16:56 +0300

    8 test passed

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-// write code here
+/* eslint-disable no-console */
+const fs = require('fs');
+
+async function moveFile() {
+  const [oldPath, newPath] = process.argv.slice(2);
+
+  if (!oldPath || !newPath) {
+    console.error('Please provide two params');
+
+    return;
+  }
+
+  fs.rename(oldPath, newPath, (error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+}
+
+moveFile();
